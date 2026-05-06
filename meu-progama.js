a29bd4f3c0926d7a430de27911502282ff088122
Newton Brito Santos <newton62574186@edu.df.senac.br>
2026-05-06 17:39:34 -0300

    06/05

---
+++ meu-progama.js
@@ -0,0 +1,6 @@
+let nome = window.prompt("digite seu nome: ")
+let anoNascimento = window.prompt("digite o ano do seu nascimento:")
+let idade = 2026-anoNascimento
+
+window.alert("Seu nome é " + nome + " e voce tem "+idade+" anos.")
+window.alert(`Seu nome é ${nome} e voce tem ${idade} anos.`)
---
+++ script.js
@@ -0,0 +1,18 @@
+
+let nomeAluno = window.prompt("Qual é o seu nome?")
+window.alert(nomeAluno)
+window.alert(1000)
+
+ var nome = "maria" //String - Cadeia de Caracteres
+ let idadeDaPessoa = 20 // int -number
+ let turmaSenacTecnicoInformaticaGama = '20261035' //String
+ let informatica = 20261035 //int / Interger (Inteiro)
+ let peso = 54.5 //float (ponto flutuante / real)
+ const TEM_AULA_SEXTA = false // 0
+ let temAulaAmanha  = true // 1
+
+ window.alert("tchau Professor :(")
+window.alert("ola mundo")
+window.confirm("voce quer aprender JavaScript")
+window.prompt('qual é o seu nome?')
+  console.log("Olá Mundo!")
